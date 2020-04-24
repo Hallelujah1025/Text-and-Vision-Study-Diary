93d83f1e967fef22be6b2cc81a7d52b92e9331ac
Haojia <690439395@qq.com>
2020-04-24 21:10:00 +0800

    Update day6Lab.py

--- day6Lab.py
+++ day6Lab.py
@@ -1,16 +1,99 @@
-corpus = ['king is a strong man',
-          'queen is a wise woman',
-          'boy is a young man',
-          'girl is a young woman',
-          'prince is a young king',
-          'princess is a young queen',
-          'man is strong',
-          'woman is pretty',
-          'prince is a boy will be king',
-          'princess is a girl will be queen']
+from nltk import ne_chunk, pos_tag, word_tokenize
+from nltk.tree import Tree
+import nltk
+import os
+
+# path of the specific dir I put in my own computer
+ref = "D:/STUDY/Text and Vision Intelligence/Assignment1/articles"
+
+def get_continuous_chunks(text):
+    chunked = ne_chunk(pos_tag(word_tokenize(text)))
+    prev = None
+    continuous_chunk = []
+    current_chunk = []
+    #print(chunked)
+    for i in chunked:
+        if type(i) == Tree:
+            current_chunk.append(" ".join([token for token, pos in i.leaves()]))
+        elif current_chunk:
+            named_entity = " ".join(current_chunk)
+            if named_entity not in continuous_chunk:
+                continuous_chunk.append(named_entity)
+                current_chunk = []
+        else:
+            continue
+
+    if continuous_chunk:
+        named_entity = " ".join(current_chunk)
+        if named_entity not in continuous_chunk:
+            continuous_chunk.append(named_entity)
+
+    return continuous_chunk
 
 
-# Define a method to remove stop words
+# a function to read all articles below a dir and convert them to one single str
+def readArticlesAsStr(ref):
+    dirs = os.listdir(ref)
+    articles = []
+    # traverse all files below the dir
+    for file in dirs:
+        with open(os.path.join(ref, file)) as f:
+            lines = []
+            # read each line and put them in the list lines[]
+            for line in f.readlines():
+                # drop unnecessary char
+                line = line.strip('\n')
+                line = line.strip('\'')
+                lines.append(line)
+            # transfer the list in one article to a str
+            strArticle = ' '.join(lines)
+        articles.append(strArticle)
+    # transfer the list included all content to one single str
+    strArticles = ' '.join(articles)
+    return strArticles
+
+txtStr = readArticlesAsStr(ref)
+# print(get_continuous_chunks(txtStr))
+
+
+count = {}
+for sent in nltk.sent_tokenize(txtStr):    # divide txt in sentences and traverse each sent
+    for chunk in nltk.ne_chunk(nltk.pos_tag(nltk.word_tokenize(sent))):
+        if hasattr(chunk, 'label'):
+            if chunk.label() in count:
+                count[chunk.label()] += 1
+            else:
+                count[chunk.label()] = 1
+print(count)
+
+
+def readArticlesAsList(ref, detail = []):
+    articles = []
+    dirs = os.listdir(ref)
+    for file in dirs:
+        article = []
+        with open(os.path.join(ref, file)) as f:
+            for line in f.readlines():
+                line = line.strip('\n')
+                line = line.strip('\'')
+                article.append(line)
+        articles.append(article)
+
+        # transform all content into one single list
+        for i in range(len(articles)):
+            for j in range(len(articles[i])):
+                detail.append(articles[i][j])
+
+txtList = []
+readArticlesAsList(ref, txtList)
+
+
+
+
+
+
+
+
 import nltk
 def remove_stop_words(corpus):
     wantedTag = set(['NN', 'NNS', 'NNP', 'NNPS', 'VB', 'VBD', 'VBG', 'VBN', 'VBP', 'VBZ'])
@@ -24,7 +107,7 @@ def remove_stop_words(corpus):
         results.append(" ".join(tmp))
     return results
 
-corpus = remove_stop_words(corpus)
+corpus = remove_stop_words(txtList)
 words = []
 for text in corpus:
     for word in text.split(' '):
